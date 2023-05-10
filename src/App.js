9d2cbab4c37c984522e90fb25962ac706c729047
Dasha0507 <93385654+Dasha0507@users.noreply.github.com>
2023-05-10 21:35:52 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+import Slider from './components/slider/slider';
+import PopularCards from './components/popular-cards/popular-cards';
+import React, { Fragment, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import {
+  collection,
+  getDocs,
+} from "firebase/firestore";
+import database from './config';
+import ArticleList from './components/article-list/article-list';
+
+
+function App() {
+  const articlesCollectionRef = collection(database, "articles");
+  const getArticles = async () => {
+    const data = await getDocs(articlesCollectionRef);
+    return data;
+  }
+
+  useEffect( ()=>{
+   getArticles().then((data) => console.log(data));
+  })
+  return (
+    <div className="App">
+      <Header/>
+      <Routes>
+      <Route
+          path="/"
+          element={
+            <Fragment>
+              <Slider/>
+              <PopularCards/>
+            </Fragment>
+          }
+        />
+        <Route path='/articles' element={
+          <ArticleList/>
+        }/>
+      </Routes>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
